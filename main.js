df3b6355fa8bebeac5d71d80a66c2da7b5eee8b2
jaanpre <10728364+jaanpre@users.noreply.github.com>
2021-03-14 21:22:16 +0100

    El penjat falles

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
 window.onload = () => {
+  const wrongLetterAudioElement = document.getElementById("wrong-letter");
+  const okWordAudioElement = document.getElementById("ok-word");
 
   let startTimer = new Date();
 
-  const proverbs = ['The squeaky wheel gets the grease', 'Better safe than sorry', 'Great minds think alike', 'Haste makes waste', 'Look before you leap', 'Measure twice cut one', 'Misery loves company', 'Better late than never', 'Mind your own business', 'Time is money', 'No man is an island', 'Fortune favors the bold', 'Hope for the best but prepare for the worst', 'A picture is worth a thousand words', 'Discretion is the greater part of valor'];
+  const falles = ['Monument faller', 'Ninot indultat', 'Nit del foc', 'Focs artificials', 'Xocolate amb xurros', 'Estendard', 'Fallera i faller', 'Pinta', 'Senyera', 'Bunyols', 'Dolçaina i tabalet', 'Banda de música', 'Mascletà', 'Ofrena de flors', 'Crida', 'Despertà', 'Manteleta', 'Cercaviles', 'Albaes', 'Casal faller', 'Artista faller', 'Pólvora', 'Terratrèmol', 'Tro de bac', 'Fallera major', 'Llibret faller', 'Concurs de paelles', 'Cavalcada del ninot', 'Entrega de premis', 'Monyos', 'Mocador'];
+  
 
-  const actors = ['Robert De Niro', 'Morgan Freeman', 'Kevin Spacey', 'Clint Eastwood', 'Tom Hanks', 'Brad Pitt', 'Mel Gibson', 'Johnny Depp', 'Nicolas Cage', 'Jack Nicholson', 'Chris Evans', 'Channing Tatum', 'George Clooney', 'Jared Leto', 'Ryan Reynolds'];
-
-  const actresses = ['Angelina Jolie', 'Emma Watson', 'Jodie Foster', 'Julia Roberts', 'Jennifer Lawrence', 'Cameron Diaz', 'Jennifer Aniston', 'Salma Hayek', 'Mila Kunis', 'Margot Robbie', 'Scarlett Johansson', 'Emma Stone', 'Cara Delevingne', 'Melissa Mccarthy', 'Charlize Theron'];
-
-  const movies = ['Forrest Gump', 'Joker', 'El Camino A Breaking Bad Movie', 'The Shawshank Redemption', 'The Green Mile', 'Intouchables', 'Pulp fiction', 'Fight Club', 'Inception', 'Gran Torino', 'Titanic', 'Seven Pounds', 'Inglourious Basterds', 'A Beautiful Mind', 'Cast Away', 'Django Unchained'];
-
-  const categories = [proverbs, actors, actresses, movies];
+  const categories = [falles];
   const category = Math.floor(Math.random() * categories.length);
   const index = Math.floor(Math.random() * categories[category].length);
-  const categoryName = ['PROVERBS', 'ACTORS', 'ACTRESSES', 'MOVIES'];
+  const categoryName = ['FALLES'];
 
-  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
+  const alphabet = 'ABCÇDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
   const password = categories[category][index].toUpperCase().split('');
   document.querySelector('.category').textContent = categoryName[category];
 
@@ -44,8 +41,8 @@ window.onload = () => {
     letter = e.target.textContent;
 
     alphabet.forEach((item, index) => {
-      if (letter === password[index]) {
-        string.textContent = string.textContent.replaceAt(index, letter);
+      if (password[index] && letter === password[index].normalize('NFD').replace(/[\u0300-\u036f]/g, "")) {
+        string.textContent = string.textContent.replaceAt(index, password[index]);
         check = true;
       }
     })
@@ -55,9 +52,11 @@ window.onload = () => {
       counter++;
       e.target.classList.add('clicked');
       image.src = `img/s${counter}.png`;
+	  wrongLetterAudioElement.play();
     }
 
     if (string.textContent == password.join('')) {
+	  okWordAudioElement.play();
       image.src = `img/s8.png`;
       let stopTimer = new Date();
 
@@ -65,16 +64,16 @@ window.onload = () => {
       let minutes = parseInt(seconds / 60);
       seconds = parseInt(seconds % 60);
 
-      document.querySelector('ul').innerHTML = `You won, congratulations! Your time: <span class="time">${minutes}min ${seconds}s</span>.
-    <div class="reset">ANOTHER ONE?<br/>
+      document.querySelector('ul').innerHTML = `Has guanyat, enhorabona! Has tardat: <span class="time">${minutes}min ${seconds}s</span>.
+    <div class="reset">AlTRA PARAULA?<br/>
     <img class="tryAgain" src="img/spin.png"/></div>`;
       document.querySelector('.tryAgain').addEventListener('click', () => {
         window.location.reload();
       })
 
     } else if (counter >= 7) {
-      document.querySelector('ul').innerHTML = `You're dead! The password is: <span style='color:red'>${password.join('')}</span>
-    <div class="reset">Try again!<br/>
+      document.querySelector('ul').innerHTML = `Has fallat! La paraula és: <span style='color:red'>${password.join('')}</span>
+    <div class="reset">Torna a intentar-ho!<br/>
     <img class="tryAgain" src="img/spin.png"/></div>`;
       document.querySelector('.tryAgain').addEventListener('click', () => {
         window.location.reload();
